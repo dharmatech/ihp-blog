b42ddd60c2d75a855df7114b06b9092d623238f4
Ed Cavazos <wayo.cavazos@gmail.com>
2021-05-17 21:18:22 -0700

    Posts - isMarkdown

--- Web/Controller/Posts.hs
+++ Web/Controller/Posts.hs
@@ -6,6 +6,8 @@ import Web.View.Posts.New
 import Web.View.Posts.Edit
 import Web.View.Posts.Show
 
+import qualified Text.MMark as MMark
+
 instance Controller PostsController where
     action PostsAction = do
         posts <- query @Post
@@ -56,3 +58,11 @@ instance Controller PostsController where
 buildPost post = post
     |> fill @["title","body"]
     |> validateField #title nonEmpty
+    |> validateField #body nonEmpty
+    |> validateField #body isMarkdown
+
+isMarkdown :: Text -> ValidatorResult
+isMarkdown text =
+    case MMark.parse "" text of
+        Left _ -> Failure "Please provide valid Markdown"
+        Right _ -> Success
