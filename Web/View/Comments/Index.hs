2475a4ecb419df492690ba840058c2b809699f01
Ed Cavazos <wayo.cavazos@gmail.com>
2021-08-08 03:01:45 -0700

    Comments Index - NewCommentAction experiment

--- Web/View/Comments/Index.hs
+++ Web/View/Comments/Index.hs
@@ -10,7 +10,22 @@ instance View IndexView where
                 <li class="breadcrumb-item active"><a href={CommentsAction}>Comments</a></li>
             </ol>
         </nav>
+
         <!-- <h1>Index <a href={pathTo NewCommentAction} class="btn btn-primary ml-4">+ New</a></h1> -->
+
+        <!-- <h1>Index <a href={NewCommentAction ""} class="btn btn-primary ml-4">+ New</a></h1> -->
+
+        <!-- <h1>
+            Index
+            <a
+                href={NewCommentAction "0f671934-0971-402e-ab70-f17e91b56a82"}
+                class="btn btn-primary ml-4">
+                + New bcd
+            </a>
+        </h1> -->
+
+        <!-- <a href={NewCommentAction "0f671934-0971-402e-ab70-f17e91b56a82"}>abc</a> -->
+        
         <h1>Comments</h1>
         <div class="table-responsive">
             <table class="table">
